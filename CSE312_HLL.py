9aae29ce9d2fafba06d756421e3aed5215856f61
Vikram Guhan Subbiah <vsubbiah@uw.edu>
2020-08-13 22:02:25 +0530

    Initial Commit with HyperLogLog file template

---
+++ CSE312_HLL.py
@@ -0,0 +1,34 @@
+# Authors: Vikram Guhan Subbiah, Alexander Hughes
+# Date: 13th August 2020
+# Course: CSE 312 20su
+# Title: Final Project
+# Instructor: Alex Tsun
+
+# HyperLogLog
+# ---------------------
+# A data structure to count distinct elements
+# with higher efficiency using a low amount of
+# vairance.
+# ---------------------
+
+# Import statements
+import numpy as np
+import matplotlib.pyplot as plt
+
+# HyperLogLog 
+class Hll312():
+	# TODO: Add default m value and filename
+	def __init__(self, m:int, filename:string):
+		pass
+
+	# TODO: implement add method
+	def add(v: string):
+
+	# TODO: implement count method
+	def count(self) -> int:
+
+	# TODO: implement merge method
+	def merge(self, other:Hll312):
+
+if __name__ == '__main__':
+	# TODO: add main method testing and plotting
