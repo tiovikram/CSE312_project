9c61fb4d4d435323cf087b99cd353e267351048c
Vikram Guhan Subbiah <vsubbiah@uw.edu>
2020-08-14 08:04:01 +0530

    __init__ function in progress. Need to decide relationship between hashing function output length and table structure

--- CSE312_HLL.py
+++ CSE312_HLL.py
@@ -18,12 +18,20 @@ import matplotlib.pyplot as plt
 # HyperLogLog 
 class Hll312():
 	# TODO: Add default m value and filename
-	def __init__(self, m:int, filename:string):
-		pass
+	def __init__(self, hash_size:int, m:int, filename:str):	
+		self.hash_size = hash_size
+		self.m = m
+		self.table = np.zeros(shape((int)np.log2(m), m))
+		self.tests = np.genfromtext(filename, dtype=str)
+
+		self.bucket_bitmap = np.concatenate((np.ones(shape(hash_size), np.zeros(shape(m)), axis=None)
+		self.value_bitmap = np.logical_not(self.bucket_bitmap)
 
 	# TODO: implement add method
-	def add(v: string):
-
+	def add(self, v: str):
+		hashed_data = __hash(input=v, hash_size=self.hash_size)
+		bucket = np.logical_and(hashed_data, self.bucket_bitmap)
+			
 	# TODO: implement count method
 	def count(self) -> int:
 
@@ -31,10 +39,10 @@ class Hll312():
 	def merge(self, other:Hll312):
 
 	# Private hashing function
-	# input: string of arbitrary length
+	# input: str of arbitrary length
 	# hash_size: the length of the hashed output
 	# return: an 1-dimensional array of m boolean elements
-	def __hash(input:string, hash_size:int) -> ndarray:
+	def __hash(input:str, hash_size:int):
 
 if __name__ == '__main__':
 	# TODO: add main method testing and plotting
